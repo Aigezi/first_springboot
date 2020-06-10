f41f5ab7f63e9655bc0c2bdfc1979437fddfe375
aigezi <18272696305@163.com>
2020-06-10 12:55:08 +0800

    first commit

---
+++ second_springboot/src/main/java/com/aigezi/second/SecondSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.aigezi.second;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondSpringbootApplication.class, args);
+    }
+
+}
